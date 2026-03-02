b2f850922e0f8b4e67ca4b36781ecfb9fb5ea094
Emilia Franzén West <eft24002@student.mdu.se>
2026-03-02 23:13:07 +0100

    Task 2

---
+++ Program5.cs
@@ -0,0 +1,57 @@
+﻿/*using System;
+
+class Program
+{
+
+	static void Test(Action block)
+	{
+		try
+		{
+			block();
+		}
+		//ADD MORE CATCH BLOCKS HERE 
+		catch (Exception)
+		{
+			Console.WriteLine("generic exception");
+		}
+	}
+
+	static void DivCase()
+	{
+		IVisitable exp = new Div(new Literal(120), new Add(new Literal(8), new Literal(4)));
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+		exp = new Div(new Mul(new Literal(5), new Literal(4)), new Literal(0));
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+	}
+
+	static void FctCase()
+	{
+		IVisitable exp = new Fct(new Mul(new Literal(2), new Literal(3)));
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+		exp = new Fct(new Mul(new Literal(-2), new Literal(7)));
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+	}
+
+	static void MaxCase()
+	{
+		IVisitable[] args = new IVisitable[] { new Add(new Literal(-2), new Literal(9)), new Literal(6), new Literal(0) };
+		IVisitable exp = new Max(args);
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+		args = new IVisitable[0];
+		exp = new Max(args);
+		Console.Write(new Stringifier(exp) + " = ");
+		Console.WriteLine(new Evaluator(exp));
+	}
+
+	static void Main()
+	{
+		Test(DivCase);
+		Test(FctCase);
+		Test(MaxCase);
+	}
+}*/
